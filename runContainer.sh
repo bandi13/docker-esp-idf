6d0f16579e8cdfbdec591455883371d08097c192
Andras Fekete <anfekete@silabs.com>
2019-08-07 09:56:01 -0400

    Make run script more user friendly

--- runContainer.sh
+++ runContainer.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-# Set this to your ESP project directory
-export ESP_PROJECT=~/githubs/esp32-80211-tx
+if [ -z $1 ]; then
+  echo "Usage: $0 <pathToYourProject>"
+  exit 1
+fi
 
-docker pull bandi13/esp-idf
+# Set this to your ESP project directory
+ESP_PROJECT=$1
+
+CONTAINER_NAME=bandi13/esp-idf
+docker pull ${CONTAINER_NAME}
 
 # Create cache for built objects
 docker volume create ccache
 
+RUN_ARGS="--rm -it -e CCACHE_DIR=/ccache -v ccache:/ccache -v ${ESP_PROJECT}:/esp/project"
+
+if [ ! -e "/dev/ttyUSB0" ]; then
+  echo "No USB device for ESP32 found. You won't be able to flash the part."
+  sleep 1
+else
+  RUN_ARGS+="--device /dev/ttyUSB0"
+fi
+
 # Run the container
-docker run --rm -it -e CCACHE_DIR=/ccache -v ccache:/ccache --device /dev/ttyUSB0 -v ${ESP_PROJECT}:/esp/project bandi13/esp-idf
+docker run ${RUN_ARGS} ${CONTAINER_NAME}
